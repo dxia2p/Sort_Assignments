13e0875dda3228553e18e3aef13c151afb155af4
dxia2p <d.xia111@gmail.com>
2024-02-28 10:00:00 -0700

    asdas

---
+++ main.py
@@ -0,0 +1,10 @@
+nums = [10, 70, 30, 100, 40, 45, 90, 80, 85];
+words = ["dog","at", "good", "eye", "cat", "ball", "fish"];
+
+def bubbleSort(arr):
+    sorted(arr)
+
+bubbleSort(nums)
+bubbleSort(words)
+print(nums)
+print(words)
