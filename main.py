d2e4f2ae0be7cdf9775d7e50936b4196c5847639
dxia2p <d.xia111@gmail.com>
2024-03-05 09:47:36 -0700

    fixed sort functions

--- main.py
+++ main.py
@@ -2,43 +2,50 @@ nums = [10, 70, 30, 100, 40, 45, 90, 80, 85]
 words = ["dog","at", "good", "eye", "cat", "ball", "fish"]
 
 def bubbleSort(arr):
-    for i in range(len(arr), 0, -1):
+    for i in range(len(arr), 1, -1):
         for j in range(1, i):
             if arr[j - 1] > arr[j]:
                 temp = arr[j]
                 arr[j] = arr[j - 1]
                 arr[j - 1] = temp
-    return arr
 
-print(bubbleSort(nums))
-print(bubbleSort(words))
+bubbleSort(nums)
+print(nums)
+bubbleSort(words)
+print(words)
 
+nums = [10, 70, 30, 100, 40, 45, 90, 80, 85]
+words = ["dog","at", "good", "eye", "cat", "ball", "fish"]
 
 def selectionSort(arr):
     for i in range(0, len(arr) - 1):
         minPos = i
-        for j in range(i, len(arr)):
+        for j in range(i + 1, len(arr)):
             if arr[j] < arr[minPos]:
-                minPos = i
+                minPos = j
         temp = arr[i]
         arr[i] = arr[minPos]
         arr[minPos] = temp
-    return arr
+    
+selectionSort(nums)
+print(nums)
+selectionSort(words)
+print(words)
 
-print(selectionSort(nums))
-print(selectionSort(words))
+nums = [10, 70, 30, 100, 40, 45, 90, 80, 85]
+words = ["dog","at", "good", "eye", "cat", "ball", "fish"]
 
 def insertionSort(arr):
     for i in range(1, len(arr)):
         valueToInsert = arr[i]
-        insertPos = i - 1
+        insertPos = i
 
-        while i > 0 and arr[insertPos] > valueToInsert:
-            temp = arr[insertPos]
-            arr[insertPos] = valueToInsert
-            arr[i] = temp
-        
+        while insertPos > 0 and arr[insertPos - 1] > valueToInsert:
+            arr[insertPos] = arr[insertPos - 1]
+            insertPos -= 1
+        arr[insertPos] = valueToInsert
     return arr
-
-print(insertionSort(nums))
-print(insertionSort(words))
+insertionSort(nums)
+print(nums)
+insertionSort(words)
+print(words)
